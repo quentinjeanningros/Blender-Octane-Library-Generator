aad21eb7fb05b7661f606b3a113f49bbb14c61f8
quentinjeanningros <quentin.jeanningros@hotmail.fr>
2024-03-08 13:12:25 +0100

    fix

--- interface.py
+++ interface.py
@@ -28,44 +28,6 @@ class CUSTOM_OT_GenerateShaderCatalog(bpy.types.Operator):
         self.create_shaders_tree(selected_folder)
         return {'FINISHED'}
 
-    def find_textures(self, directory):
-        # Prepare sockets based on user settings
-        sockets = [
-            ['base_color', bpy.context.scene.base_color.split(), None],
-            ['metallic', bpy.context.scene.metallic.split(), None],
-            ['specular', bpy.context.scene.specular.split(), None],
-            ['normal', bpy.context.scene.normal.split(), None],
-            ['bump', bpy.context.scene.bump.split(), None],
-            ['roughness', bpy.context.scene.roughness.split(), None],
-            ['gloss', bpy.context.scene.gloss.split(), None],
-            ['displacement', bpy.context.scene.displacement.split(), None],
-            ['transmission', bpy.context.scene.transmission.split(), None],
-            ['emission', bpy.context.scene.emission.split(), None],
-            ['alpha', bpy.context.scene.alpha.split(), None],
-            ['ambient_occlusion', bpy.context.scene.ambient_occlusion.split(), None]
-        ]
-
-        texture_files = {socket[0]: [] for socket in sockets}  # Initialize texture_files dictionary
-
-        # Collect all files in the directory that match the supported image formats
-        files = []
-        for root, _, filenames in os.walk(directory):
-            for filename in filenames:
-                if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.exr', '.hdr', '.tiff')):
-                    files.append(type('obj', (object,), {'name': os.path.join(root, filename)}))
-
-        # Match files to sockets
-        match_files_to_socket_names(files, sockets)
-
-        # Populate the texture_files dictionary based on matched sockets
-        for socket in sockets:
-            if socket[2]:  # If files were matched to this socket
-                texture_type = socket[0]
-                for matched_file_path in socket[2]:  # Iterate through matched files (now directly using the paths)
-                    texture_files[texture_type].append(matched_file_path)  # Append the file path directly
-
-        return texture_files
-
     def create_shaders_tree(self, folder_path):
         use_catalog_tree = bpy.context.scene.use_catalog_tree
         use_tags = bpy.context.scene.use_tags
